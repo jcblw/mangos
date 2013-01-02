9bd2b1ad0bc2fc5f8f86a241abb95db5c999fe5d
jacoblwe20 <jacoblowe2.0@gmail.com>
2013-01-01 22:15:54 -0800

    adding initial files

---
+++ Mango.js
@@ -0,0 +1,157 @@
+var Mongo   = require('mongodb'),
+    Db      = Mongo.Db,
+    Server  = Mongo.Server;
+
+/* Mangos.js
+ * ==========================
+ * Small library to make MongoDB CRUD 
+ */
+
+/* @Constructor Mango
+ * @Param       database  String  - Datatbase or collection name
+ * @Param       host      String  - Location of MongoDB Server
+ * @Param       port      Number  - The port that the server is running at
+ */
+
+var Mango = function(database, host, port) {
+  var that = this;
+  // Create a database object
+  that.db = new Db(database, new Server(host, port, {auto_reconnect: true}, {}), {safe:true});
+  that.db.open(function(){});
+  /* @Basic-Utility
+   * @Method    error - Function that spits out error to console and callback (Keeps functions clean)
+   * @Param     err       Object    - The error spit out be MongoDB native
+   * @Param     callback  Function  - Function to pass data to  
+   */
+  that.error = function(err, callback){
+    callback(err);
+    console.log(err);
+  }; 
+  /* @Basic-Utility
+   * @Method    hex - Create hex for _id
+   * @Param     collection  Object  - A collection object from MongoDB native
+   * @Param     str         String  - String to create Hex string from
+   * @Returns   - Returns a hex string
+   */
+  that.hex = function(collection, str){
+    return collection.db.bson_serializer.ObjectID.createFromHexString(str);
+  };
+  /* @Basic-Utility
+   * @Method    getCollection - Get collection
+   * @Param     callback      Function  - Function to pass data to
+   */
+  that.getCollection = function(callback){
+    that.db.collection(database, function(err, collection){
+      if(err) that.error(err, callback);
+      else callback(null, collection);
+    });
+  };
+  /* @Read-Utility
+   * @Method    all - Get all results
+   * @Param     callback    Function  - Function to pass data to
+   */
+  that.all = function(callback){
+    that.getCollection(function(err, collection){
+      if(err) that.error(err, callback)
+      else{
+        collection.find().toArray(function(err, results){
+          if(err) that.error(err, callback)
+          else callback(null, results)
+        }); 
+      }
+    });
+  };
+  /* @Read-Utility
+   * @Method   find - Search function
+   * @Param    filter       Object    - Object to filter results with
+   * @Param    callback     Function  - Function to pass data to
+   */
+  that.find = function(filter, callback){
+    that.getCollection(function(err, collection){
+      if(err) that.error(err, callback)
+      else{
+        collection.find(filter).toArray(function(error, results){
+          if(err) that.error(err, callback)
+          else callback(null, results)
+        });
+      } 
+    })
+  };
+  /* @Create
+   * @Method  create - Creating new entries
+   * @Param   dataset   Object    - Data to insert into database
+   * @Param   callback  Function  - Function to pass data to
+   */
+  that.create = function(dataset, callback){
+    that.getCollection(function(error,collection) {
+      if( error ) callback(error)
+      else {
+        if(typeof dataset.length === "undefined")
+          dataset = [dataset];
+
+        for( var i =0;i < dataset.length; i++ ) {
+          data = dataset[i];
+          data.created_at = new Date();
+        }
+
+        collection.insert(dataset, function() {
+          callback(null, dataset);
+        });
+      }
+    });
+  };
+  /* @Read
+   * @Method  read - Read exsisting entries
+   * @Param   filter       Object    - Object to filter results with ~ Optional first argument will become callback if not object
+   * @Param   callback     Function  - Function to pass data to
+   */
+  that.read = function(filter, callback){
+    if(typeof filter === 'object'){
+      that.find(filter, function(err, results){
+        if(err) that.error(err, callback)
+        else callback(null, results)
+      });
+    }else{
+      callback = filter;
+      that.all(function(err, results){
+        if(err) that.error(err, callback)
+        else callback(null, results);
+      });
+    }
+  };
+  /* @Update
+   * @Method  update - Update entries in database
+   * @Param   dataset   Object    - Data to update in database ~ Requires id to select specific entry
+   * @Param   callback  Function  - Function to pass data to
+   */
+  that.update = function(dataset, callback){
+    this.getCollection(function(err, collection){
+      if(err) that.error(err, callback)
+      else{
+        collection.findAndModify({_id: that.hex(collection, dataset.id)}, [['_id', 'asc']], {$set: dataset}, {new:true}, function(err, results){
+          if(err) that.error(err, calback)
+          else callback(null, results)
+        })
+      }
+    })
+  };
+  /* @Delete
+   * @Method  delete - Delete entries in database
+   * @Param   id        String    - Id of specific entry
+   * @Param   callback  Function  - Function to pass data to
+   */
+  that.delete = function(id, callback){
+    that.getCollection(function(err, collection) {
+      if(err) that.error(err, callback)
+      else {
+        collection.findAndRemove({_id: that.hex(collection, id)}, function(err, results) {
+          if(err) that.error(err, callback)
+          else callback(null, results)
+        });
+      }
+    });
+  };
+
+};
+
+exports.Mango = Mango; 
---
+++ README.md
@@ -0,0 +1,5 @@
+## Mango.js
+
+Mango.js is a simple module to make MongoDB syntax CRUD. It uses and exposes controls straight from MongoDB's Native driver
+
+#### Documentation in the code!
