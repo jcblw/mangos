c0f3bb5118ab254230bfcf3750efa0cd61c950a6
Jacob Lowe <jacoblowe2.0@gmail.com>
2013-07-07 08:40:41 -0700

    removing ready state it seems to not be needed for new versions, also bumping version

--- src/collection.js
+++ src/collection.js
@@ -8,23 +8,16 @@ module.exports = function( Mango ){ // import Mangos
   * @Param     callback      Function  - Function to pass data to
   */
   Mango.prototype.collection = function( callback ){
-    if( this.ready ){
-      this.db.collection( this._collection, function( err, collection ){
-        if(err) this.error( err, callback );
-        else callback( null, collection );
-      });
-    }else{
-      var self = this;
-      setTimeout( function(){
-        self.collection( callback ); // polling
-      }, 500 );
-    }
+    this.db.collection( this._collection, function( err, collection ){
+      if(err) this.error( err, callback );
+      else callback( null, collection );
+    });
   };
 
   Mango.prototype.Collection = function( name, options ){
     this._collection = name;
     
-    if(options){
+    if(!options){
       options = {}
     }
 
@@ -32,7 +25,7 @@ module.exports = function( Mango ){ // import Mangos
     this.privateKeys = options.privateKeys || [];
     this.collectionDefaults = options.defaults || {};
 
-    return Object.create( this ); // returns a clone of this
+    return _.extend( {}, this ); // returns a clone of this
   };
 
   Mango.prototype.model = function( collection ){
